88b88f7c2e0915ce9339466fcbb98f6517fad9c9
Dan Shevlyuk <twistedplane@gmail.com>
2015-04-29 00:01:24 +0300

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+app.debug = True
+manager = Manager(app)
---
+++ manage.py
@@ -0,0 +1,9 @@
+from app import manager
+
+
+@manager.command
+def init(): 
+	print 'OK'
+
+if __name__ == '__main__':
+	manager.run()
